f5c04297cee30ed0b10e697b8d82972500cba0e4
Vital Fadeev <vital.fadeev@gmail.com>
2024-08-08 18:49:13 +0700

    upd

--- xcb_window/source/app.d
+++ xcb_window/source/app.d
@@ -1,88 +1,86 @@
 import std.stdio;
-
+alias log = writeln;
 import xcb.xcb;
 import xau;
 
 
 auto 
 get_auth (out XAuth xauth) {
-	ushort family  = xau.FamilyLocal;
-	string address = "ASUS-K53SD";
-	string number  = "0";
-	string name    = "";
-	xau.Xauth xau_auth;
-	if (xau.GetAuthByAddr (family, address, number, name, xau_auth)) {
-		xauth.from_Xauth( xau_auth );
-		return true;
-	}
+    ushort family  = xau.FamilyLocal;
+    string address = "ASUS-K53SD";
+    string number  = "0";
+    string name    = "";
+    xau.Xauth xau_auth;
+    if (xau.GetAuthByAddr (family, address, number, name, xau_auth)) {
+        xauth.from_Xauth (xau_auth);
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 struct 
 XAuth {
-	xcb_auth_info_t xcb_auth;
-	string         _name;
-	ubyte[]  	   _data;
+    xcb_auth_info_t xcb_auth;
+    string         _name;
+    ubyte[]        _data;
 
-	void 
+    void 
     from_Xauth (in xau.Xauth _auth) {
-		import std.conv : to;
+        import std.conv : to;
 
-		_name            = _auth.name.dup;
-		xcb_auth.namelen = _name.length.to!(typeof(xcb_auth_info_t.namelen));
-		xcb_auth.name    = cast(char*)_name.ptr;
-		_data            = _auth.data.dup;
-		xcb_auth.datalen = _data.length.to!(typeof(xcb_auth_info_t.namelen));
-		xcb_auth.data    = cast(char*)_data.ptr;
-	}
+        _name            = _auth.name.dup;
+        xcb_auth.namelen = _name.length.to!(typeof (xcb_auth_info_t.namelen));
+        xcb_auth.name    = cast (char*) _name.ptr;
+        _data            = _auth.data.dup;
+        xcb_auth.datalen = _data.length.to!(typeof (xcb_auth_info_t.namelen));
+        xcb_auth.data    = cast (char*) _data.ptr;
+    }
 }
 
 void 
 main () {
-	// INIT
+    // INIT
     xcb_connection_t* c;
     xcb_screen_t*     screen;
     bool              auth_flag = true;
 
     // CONNECT
     if (auth_flag) {
-	    const char* display;
-	    int         prefered_screen;
+        const char* display;
+        int         prefered_screen;
 
-	    // AUTH
-	    XAuth auth;
-	    get_auth( auth );
-	    c = xcb_connect_to_display_with_auth_info( display, &auth.xcb_auth, &prefered_screen );
-	    if ( xcb_connection_has_error( c ) )
-	        throw new XCBException( "Cannot open display", c );    	
+        // AUTH
+        XAuth auth;
+        get_auth (auth);
+        c = xcb_connect_to_display_with_auth_info (display, &auth.xcb_auth, &prefered_screen);
+        if (xcb_connection_has_error (c))
+            throw new XCBException ("Cannot open display", c);
     }
     else {
-	    // Open the connection to the X server     
-	    //c = xcb_connect( getenv( "DISPLAY" ), null );
-	    c = xcb_connect( null, null );
-	    if ( xcb_connection_has_error( c ) )
-	        throw new XCBException( "Cannot open display", c );    	
+        // Open the connection to the X server     
+        //c = xcb_connect( getenv( "DISPLAY" ), null );
+        c = xcb_connect (null, null);
+        if (xcb_connection_has_error (c))
+            throw new XCBException ("Cannot open display", c);
     }
 
     // Get the first screen
     // screen = xcb_setup_roots_iterator( xcb_get_setup( c ) )[prefered_screen].data;
-    screen = xcb_setup_roots_iterator( xcb_get_setup( c ) ).data;
+    screen = xcb_setup_roots_iterator (xcb_get_setup (c)).data;
 
-    writeln( "width x height (in pixels): ", screen.width_in_pixels, "x", screen.height_in_pixels );
+    log ( "width x height (in pixels): ", screen.width_in_pixels, "x", screen.height_in_pixels );
 
     // WINDOW
     xcb_window_t hwnd;
 
     // Ask for our window's Id
-    hwnd = xcb_generate_id( c );
-
-    writeln( "hwnd: ", hwnd );
+    hwnd = xcb_generate_id (c);
 
     //
     immutable(uint)   value_mask = 
-    	XCB_CW_BACK_PIXEL | 
-    	XCB_CW_EVENT_MASK;
+        XCB_CW_BACK_PIXEL | 
+        XCB_CW_EVENT_MASK;
     immutable(uint[]) value_list = [
         screen.white_pixel,
         XCB_EVENT_MASK_EXPOSURE |
@@ -98,36 +96,35 @@ main () {
 
     // Create the window
     auto cookie =  // xcb_void_cookie_t 
-	    xcb_create_window( 
-	        c,                             // Connection          
-	        XCB_COPY_FROM_PARENT,          // depth (same as root)
-	        hwnd,                          // window Id           
-	        screen.root,          // parent window       
-	        0, 0,                          // x, y                
-	        800, 600,                // width, height       
-	        2,                            // border_width        
-	        XCB_WINDOW_CLASS_INPUT_OUTPUT, // class               
-	        screen.root_visual,   // visual              
-	        value_mask, 						// masks
-	        value_list.ptr                  // not used yet 
-	    );                                 
+        xcb_create_window( 
+            c,                             // Connection          
+            XCB_COPY_FROM_PARENT,          // depth (same as root)
+            hwnd,                          // window Id           
+            screen.root,                   // parent window       
+            0, 0,                          // x, y                
+            512, 512,                      // width, height       
+            2,                             // border_width        
+            XCB_WINDOW_CLASS_INPUT_OUTPUT, // class               
+            screen.root_visual,            // visual              
+            value_mask,                    // masks
+            value_list.ptr                 // not used yet 
+        );                                 
 
     // Map the window on the screen
-    xcb_map_window( c, hwnd );
+    xcb_map_window (c, hwnd);
 
     // Make sure commands are sent before we pause, so window is shown
-    xcb_flush( c );
+    xcb_flush (c);
 
     // EVENT LOOP
     xcb_generic_event_t* front;
 
-    while( ( front = xcb_wait_for_event( c ) ) != null )
-    {
+    while ((front = xcb_wait_for_event (c)) != null) {
         import core.stdc.stdlib : free;
-        free( front );
+        free (front);
     }
 
-	xcb_disconnect( c );
+    xcb_disconnect (c);
 }
 
 class 
@@ -135,14 +132,14 @@ XCBException : Exception {
     import xcb.xcb;
 
     this (string s, xcb_connection_t* c) {
-        auto err = xcb_connection_has_error( c );
-        super( xcb_error_to_string( err ) );
+        auto err = xcb_connection_has_error (c);
+        super (xcb_error_to_string (err));
     }
 }
 
 string 
 xcb_error_to_string (int err) {
-    switch ( err ) {
+    switch (err) {
         case XCB_CONN_ERROR: 
             return ": [XCB_CONN_ERROR]: because of socket errors, pipe errors or other stream errors";
         case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
